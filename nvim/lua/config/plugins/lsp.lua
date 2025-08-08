b73076b88220bc7adb64aed5c26445838ae50fe3
Adriano Bacha <abacha@gmail.com>
2025-08-08 08:33:04 -0300

    updates

--- nvim/lua/config/plugins/lsp.lua
+++ nvim/lua/config/plugins/lsp.lua
@@ -1,7 +1,7 @@
 local lspconfig = require('lspconfig')
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-lspconfig.tsserver.setup({
+lspconfig.ts_ls.setup({
  capabilities = capabilities,
 })
 
