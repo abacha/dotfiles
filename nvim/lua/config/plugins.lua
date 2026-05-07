66db0312ab54213cfb2ddde67de6ff83c389827f
Adriano Bacha <abacha@gmail.com>
2026-05-07 14:24:51 -0300

    Fix MarkdownPreview

--- nvim/lua/config/plugins.lua
+++ nvim/lua/config/plugins.lua
@@ -29,7 +29,6 @@ return require('packer').startup(function(use)
   use {
     'iamcco/markdown-preview.nvim',
     run = 'cd app && npm install',
-    ft = { 'markdown' },
   }
 
   -- Testing
--- nvim/lua/config/plugins/markdown_preview.lua
+++ nvim/lua/config/plugins/markdown_preview.lua
@@ -3,3 +3,13 @@ vim.g.mkdp_auto_start = 0
 vim.g.mkdp_auto_close = 1
 vim.g.mkdp_echo_preview_url = 1
 vim.g.mkdp_open_to_the_world = 0
+
+-- SSH-aware configuration for remote nodes
+if vim.env.SSH_CONNECTION then
+  vim.g.mkdp_open_to_the_world = 1
+  vim.g.mkdp_browser = '' -- Don't try to open a browser remotely
+  vim.g.mkdp_port = '9097' -- Use a non-common port
+elseif vim.fn.has('wsl') == 1 then
+  -- Use wslview in WSL to open the browser in Windows
+  vim.g.mkdp_browser = 'wslview'
+end
